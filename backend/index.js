25b59af2fd8afa1f735ce5cfef69b6849dabb68a
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-03-08 23:51:17 +0530

    User registration endpoint working and tested

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 dns.setDefaultResultOrder("ipv4first");
 
@@ -10,8 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 
