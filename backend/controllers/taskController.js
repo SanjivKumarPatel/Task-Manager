2333782cdb74a6786f3e0c0f50897d1fefb9e88d
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-03-14 22:45:01 +0530

    Add create task controller with validation

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,38 @@
+import Task from "../models/Task.js";
+export const createTask = async (req, res) => {
+  try {
+    const { title, description, status, priority, dueDate } = req.body;
+    // validation
+    if (!title || !status || !priority || !dueDate) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+    const validStatuses = ["Pending", "In-process", "Completed"];
+    if (!validStatuses.includes(status)) {
+      return res.status(400).json({ message: "Invalid Status" });
+    }
+    const validPriorities = ["High", "Medium", "Low"];
+    if (!validPriorities.includes(priority)) {
+      return res.status(400).json({ message: "Invalid priority" });
+    }
+    if (isNaN(Date.parse(dueDate))) {
+      return res.status(400).json({ message: "Invalid dueDate" });
+    }
+    const newTask = new Task({
+      title,
+      description,
+      status,
+      priority,
+      dueDate,
+      user: req.user.id,
+    });
+    const savedTask = await newTask.save();
+    res.status(201).json({
+      message: "Task created successfully",
+      task: savedTask,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error. Please try again later." });
+  }
+};
